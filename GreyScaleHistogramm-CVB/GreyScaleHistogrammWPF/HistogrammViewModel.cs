33903925d8759bbfa33466ba4892178c65ad044a
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-11-14 10:51:45 +0100

    Get the number of points which have the same greyscale value

--- GreyScaleHistogramm-CVB/GreyScaleHistogrammWPF/HistogrammViewModel.cs
+++ GreyScaleHistogramm-CVB/GreyScaleHistogrammWPF/HistogrammViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,55 +10,66 @@ using Stemmer.Cvb;
 
 namespace GreyScaleHistogrammWPF
 {
-    public class HistogrammViewModel
+  public class HistogrammViewModel
+  {
+    public PointCollection PointCollection { get; private set; } = CreatePointCollection();
+
+    private static PointCollection CreatePointCollection()
     {
-        public PointCollection PointCollection { get; private set; } = CreatePointCollection();
+      using (var image = Image.FromFile(@"C:\Users\jsiedersberger\Pictures\Saved Pictures\pexels-photo-285286.jpeg"))
+      {
+        var histogram = CreateHistogram(image);
+      }
 
-        private static PointCollection CreatePointCollection()
-        {
-            List<int> greyScaleValues = new List<int>();
-            CopyPixelsWithValue(Image.FromFile(@"C:\Users\jsiedersberger\Pictures\Saved Pictures\pexels-photo-285286.jpeg"), greyScaleValues);
-            int max = greyScaleValues.Max();
-
-            PointCollection points = new PointCollection();
-            // first point (lower-left corner)
-            points.Add(new Point(0, max));
-            // middle points
-            for (int i = 0; i < greyScaleValues.Count(); i++)
-            {
-                points.Add(new Point(i, max - greyScaleValues[i]));
-            }
-            // last point (lower-right corner)
-            points.Add(new Point(greyScaleValues.Count() - 1, max));
-            return points;
-        }
-
-        private static void CopyPixelsWithValue(Image source, List<int> values)
-        {
-            
-            unsafe
-            {
-                int width = source.Width;
-                int height = source.Height;
-
-                var linearAccess = source.Planes[0].GetLinearAccess();
-                var basePtr = linearAccess.BasePtr;
-
-                int xInc = (int)linearAccess.XInc;
-                int yInc = (int)linearAccess.YInc;
-
-                for (int y = 0; y < height; y++)
-                {
-                    if (y >= 1)
-                        return;
-                    var line = (byte*)basePtr.ToPointer() + y * yInc;
-                    for (int x = 0; x < width; x++)
-                    {
-                        var pixel = *(line + x * xInc);
-                        values.Add(pixel);
-                    }
-                }
-            }
-        }
+      return null;
     }
+
+    private static int[] CreateHistogram(Image image)
+    {
+      Debug.Assert(image != null);
+      Debug.Assert(image.Planes[0].DataType == DataTypes.Int8BppUnsigned);
+
+      var histogram = new int[byte.MaxValue + 1];
+
+      var access = image.Planes[0].GetLinearAccess<byte>();
+
+      var size = image.Size;
+      for (int y = 0; y < size.Height; y++)
+      {
+        for (int x = 0; x < size.Width; x++)
+        {
+          var pixelValue = access[x, y];
+          ++histogram[pixelValue];
+        }
+      }
+
+      return histogram;
+    }
+
+    private static void CopyPixelsWithValue(Image source, List<int> values)
+    {
+
+      unsafe
+      {
+        int width = source.Width;
+        int height = source.Height;
+
+        var linearAccess = source.Planes[0].GetLinearAccess();
+        var basePtr = linearAccess.BasePtr;
+
+        int xInc = (int)linearAccess.XInc;
+        int yInc = (int)linearAccess.YInc;
+
+        for (int y = 0; y < /*height*/1; y++)
+        {
+          var line = (byte*)basePtr.ToPointer() + y * yInc;
+          for (int x = 0; x < width; x++)
+          {
+            var pixel = *(line + x * xInc);
+            values.Add(pixel);
+          }
+        }
+      }
+    }
+  }
 }
