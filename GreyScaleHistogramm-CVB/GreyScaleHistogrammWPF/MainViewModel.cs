db2e074797e0606743c2c053eb9242ce3bc1f351
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-11-22 11:39:39 +0100

    Calculate Processing Time, Create MainWindow with ProcessingTime and Histogramm

--- GreyScaleHistogramm-CVB/GreyScaleHistogrammWPF/MainViewModel.cs
+++ GreyScaleHistogramm-CVB/GreyScaleHistogrammWPF/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Stemmer.Cvb;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,11 @@ namespace GreyScaleHistogrammWPF
     {
       using (var image = Image.FromFile(@"C:\Users\jsiedersberger\Pictures\Saved Pictures\lamborghini.jpg"))
       {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
         Data = Histogram.Create(image);
+        stopwatch.Stop();
+        Time = stopwatch.ElapsedMilliseconds.ToString() + "ms";
       }
     }
 
@@ -21,5 +26,7 @@ namespace GreyScaleHistogrammWPF
     /// Contains how often a color value appeard in the image
     /// </summary>
     public int[][] Data { get; private set; }
+
+    public string Time { get; set; }
   }
 }
